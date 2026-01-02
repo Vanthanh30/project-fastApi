a4f650c8bbed936c8daf154166c037d607e5fea2
ADMIN <nththuong2505@gmail.com>
2026-01-02 11:41:42 +0700

    admin_products

---
+++ backend/app/routers/product.py
@@ -0,0 +1,56 @@
+
+from datetime import datetime 
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.base import get_db
+from app.models.product import Product
+from app.schemas.product import ProductCreate, ProductResponse, ProductUpdate
+from app.models.category import Category
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+@router.post("/", response_model=ProductResponse)
+def create_product (data: ProductCreate, db: Session = Depends(get_db)):
+    category = db.query(Category).filter(Category.id == data.category_id).first()
+
+    if not category:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found"
+        )
+    product = Product(**data.model_dump(exclude_unset=True))
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
+
+@router.get("/", response_model=list[ProductResponse])
+def get_all_products(db:Session = Depends(get_db)):
+    products = db.query(Product).filter(Product.deleted_at.is_(None)).all()
+    return products
+
+@router.get("/{product_id}", response_model= ProductResponse)
+def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id, Product.deleted_at.is_(None)).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    return product
+
+@router.put("/{product_id}", response_model=ProductResponse)
+def update_product(product_id: int, data: ProductUpdate, db: Session = Depends(get_db)):
+    product = get_product_by_id(product_id, db)
+    for key, value in data.model_dump(exclude_unset=True).items():
+        setattr(product, key, value)
+
+    db.commit()
+    db.refresh(product)
+    return product
+
+@router.delete("/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    product = get_product_by_id(product_id, db)
+    product.deleted_at = datetime.now()
+    db.commit()
+    return {"message": "Delete product success"}
--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from app.db.base import engine
 from app.routers.auth import router as auth_router
 from app.routers.admin import router as admin_router
 from app.routers.category import router as category_router
+from app.routers.product import router as product_router
 import app.core.cloudinary_config
 from app.utils.seed_admin import seed_admin
 app = FastAPI()
@@ -14,6 +15,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(category_router)
+app.include_router(product_router)
 seed_admin()
 
 @app.get("/")
